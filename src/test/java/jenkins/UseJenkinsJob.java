5302a915d3275247b86b6542161140a2ec0a6e29
Ilya-Besedin <besedin.work@gmail.com>
2022-04-13 00:37:29 +0300

    del TestBase.java

--- src/test/java/jenkins/UseJenkinsJob.java
+++ src/test/java/jenkins/UseJenkinsJob.java
@@ -1,11 +1,31 @@
 package jenkins;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import helpers.StepsAnnotation;
 import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class UseJenkinsJob extends TestBase {
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class UseJenkinsJob {
+    public void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1280x800";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
 
     public static final String repository = "Ilya-Besedin/Homework-6";
 
@@ -24,5 +44,14 @@ public class UseJenkinsJob extends TestBase {
         steps.searchForRepository(repository);
         steps.openRepository();
         steps.checkTabName();
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
